adda169ebe69c1de80cc31f0da8fd5f10fe85d20
shawnbae <tngks0315@naver.com>
2020-11-17 19:14:38 +0900

    20201117 update

--- 완전탐색/20201113_소수찾기.py
+++ 완전탐색/20201113_소수찾기.py
@@ -1,2 +1,13 @@
 # 문제출처: https://programmers.co.kr/learn/courses/30/lessons/42839
 
+
+    sosu_list= []
+    for i in range(2,10000000):
+        for j in range(2, 10000000):
+            if i%j != 0:
+                sosu_list.append(i)
+            else:
+                continue
+              
+        
+from collections import defaultdict
