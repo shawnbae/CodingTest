1772dffb3ac7e929fd4738b0fa0fadf84b5f75b0
soohan <tngks0315@naver.com>
2022-01-23 19:30:01 +0900

    20220123 update

--- solutions/신고 결과 받기/solution.py
+++ solutions/신고 결과 받기/solution.py
@@ -1,11 +1,27 @@
-# 문제출처: https://programmers.co.kr/learn/courses/30/lessons/42576
+# 문제출처: https://programmers.co.kr/learn/courses/30/lessons/92334
 
-from collections import Counter
+from itertools import chain
+from collections import Counter, defaultdict
 
-def solution(participant, completion):
-    pardict= Counter(participant)
-    comdict= Counter(completion)
+def solution(id_list, report, k):
+    from_to = {}
+    for rep in report:
+        from_to.setdefault(rep.split(" ")[0], set()).add(rep.split(" ")[1])
     
-    answer = ''.join((pardict - comdict).keys())
-        
-    return answer
+    reported_list = [key for key, val in Counter(chain(*from_to.values())).items() if val >= k]
+
+    def name_to_num(list):
+        num = 0
+        for li in list:
+            if li in reported_list:
+                num += 1
+        return num
+    
+    answer_dict = defaultdict(int)
+    for i in id_list:
+        if i in from_to:
+            answer_dict[i] = name_to_num(from_to[i])
+        else:
+            answer_dict[i] = 0
+            
+    return list(answer_dict.values())
