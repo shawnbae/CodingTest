b4c2b0800c9b444f8d4e76a85176e69e9823e35f
soohan <tngks0315@naver.com>
2022-01-29 18:35:41 +0900

    20220129 update

--- solutions/입국 심사/solution.py
+++ solutions/입국 심사/solution.py
@@ -1,12 +1,1 @@
-# 문제출처: https://programmers.co.kr/learn/courses/30/lessons/43165
-
-from itertools import product
-
-def solution(numbers, target):
-    ziplist = [(x, -x) for x in numbers]
-    answer = 0
-    for prod in product(*ziplist):
-        if sum(prod) == target:
-            answer += 1
-    
-    return answer
+# 문제출처: https://programmers.co.kr/learn/courses/30/lessons/43238
